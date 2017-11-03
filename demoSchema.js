ff2ffb31760d56309ab0b0fee3c6a359969f346d
Allison Kahn <allisonrkahn@ufl.edu>
2017-11-03 16:18:53 -0400

    database

---
+++ demoSchema.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+    var demographicSchema = new mongoose.Schema({
+      zip: Number,
+      population: Number,
+      medianAge: Number,
+      numOfHousing: Number,
+      medianIncome: Number,
+      percentBelowPov: Number,
+      race: {
+          whiteAlone: Number,
+          AAAlone: Number,
+          nativeAm: Number,
+          asian: Number,
+          nativeHawaiian: Number,
+          Other: Number,
+          twoOrMore: Number,
+          hispanicLatino: Number,
+          whiteAloneNotHispanic: Number
+      }
+    });
+
+demographicSchema.pre('save', function(next) {
+  var currentDate = new Date();
+  this.updated_at = currentDate;
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
+var Demo = mongoose.model('Demo', demographicSchema);
+
+module.exports = Demo;
