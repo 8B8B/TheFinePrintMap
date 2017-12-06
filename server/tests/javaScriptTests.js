2e996b1445b4dd42510e64485df86ab17f51156f
matthew <msilversmith@ufl.edu>
2017-12-05 20:28:41 -0500

    added testing

---
+++ server/tests/javaScriptTests.js
@@ -0,0 +1,32 @@
+
+function getGradient() {
+  return [
+    'rgba(255, 255, 255, 0)',
+    'rgba(229, 37, 0, 1)',
+    'rgba(231, 58, 0, 1)',
+    'rgba(234, 80, 0, 1)',
+    'rgba(226, 102, 0, 1)',
+    'rgba(239, 112, 0, 1)',
+    'rgba(242, 146, 0, 1)',
+    'rgba(244, 167, 0, 1)',
+    'rgba(247, 189, 0, 1)',
+    'rgba(249, 211, 0, 1)',
+    'rgba(252, 233, 0, 1)',
+    'rgba(226, 224, 0, 1)',
+    'rgba(201, 216, 0, 1)',
+    'rgba(176, 208, 0, 1)',
+    'rgba(151, 199, 0, 1)',
+    'rgba(126, 191, 0, 1)',
+    'rgba(100, 183, 0, 1)',
+    'rgba(75, 174, 0, 1)',
+    'rgba(50, 166, 0, 1)',
+    'rgba(25, 158, 0, 1)',
+    'rgba(0, 150, 0, 1)'
+  ];
+}
+
+
+function initMap() {
+  var content32608 = '<div>' + '<p> Zip Code: 32608<br> Population: 45,832 <br> Median Age: 28<br> Number of Houses: 23,245 <br> Median Income: $40,465<br> Below Poverty Line: 29.40%</p>' + '</div>';
+  return content32608;
+}
